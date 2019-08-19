f3361c912c84676fa273ec597f2b20ddeb1e3762
DarioBrando <kirilleven@mail.ru>
2019-08-19 15:02:16 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -8,7 +8,14 @@ master = tk.Tk()
 step = 60
 N_X = 10
 N_Y = 10
-canvas = tk.Canvas(master, bg = 'green', height = step*N_X, width = step*N_Y)
+canvas = tk.Canvas(master, bg = 'blue', height = step*N_X, width = step*N_Y)
 
 player_pos = (randompos(N_X), randompos(N_Y))
 exit_pos =(randompos(N_X), randompos(N_Y)) 
+
+player = canvas.create_oval(player_pos, (player_pos[0]+step, player_pos[1]+step,), fill = 'green')
+
+exit_g = canvas.create_oval(exit_pos, (exit_pos[0]+step, exit_pos[1]+step,), fill = 'yellow')
+
+canvas.pack()
+master.mainloop()
