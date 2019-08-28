7b6d3f6348487c83d7b54bd7f72a7332574443a1
DarioBrando <kirilleven@mail.ru>
2019-08-28 14:53:42 +0300

    Update вот щас реальная игра.py

--- вот щас реальная игра.py
+++ вот щас реальная игра.py
@@ -14,6 +14,20 @@ c.move(ship, 683, 384)
 c.move(ship1, 683, 384)
 ship_spd = 10
 score = 0
-def ship_move()
-
+def ship_move(event):
+    if event.keysym == 'Up': #вверх
+        c.move(ship, 0, -ship_spd)
+        c.move(ship1, 0, -ship_sdp)
+    elif event.keysym == 'Down':#вниз
+        c.move(ship, 0, ship_spd)
+        c.move(ship1, 0, ship_spd)
+    elif event.keysym == 'Left':#влево
+        c.move(ship, -ship_spd, 0)
+        c.move(ship1, -ship_spd, 0)
+    elif event.keysym == 'c'+'h'+'e'+'a'+'t':#чит
+        score += 10000
+c.bind_all('<Key>', move_ship)
+    
+        
+    
 
