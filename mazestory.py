95421db432c2a0125e64c5bc5f11496009cad31e
DarioBrando <kirilleven@mail.ru>
2019-08-28 14:20:16 +0300

    buble game

--- mazestory.py
+++ mazestory.py
@@ -1,23 +1,6 @@
-import tkinter as tk
-import random
 
-colours = ['Red', 'Blue', 'Green', 'Pink', 'Black', 'Yellow', 'Orange','White', 'Purple', 'Brown']
 
-score = 0
 
-timeleft = 30
-
-def newgame(event):
-    if timeleft == 30:
-        countdown()
-    nextColour()
-
-def nxtclr():
-    global score
-    global timeleft
-    if timeleft>0:
-        sys.exit
-        print('Серьезно? -_-') or ('Нормально же шли :(') or ('Все с тобой ясно')
 
 
     
---
+++ вот щас реальная игра.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from math import sqrt
+from random import shuffle
+Height = 750
+Widht = 1370
+window = Tk()
+colors = ['blue', 'green', 'pink', 'purple', 'black', 'blue', 'red']
+health 
