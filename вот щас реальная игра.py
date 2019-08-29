93a2a04a36dfe1d482d3dfb4daf6449fedbddaf8
DarioBrando <kirilleven@mail.ru>
2019-08-29 22:53:57 +0300

    Update вот щас реальная игра.py

--- вот щас реальная игра.py
+++ вот щас реальная игра.py
@@ -1,19 +1,20 @@
 from tkinter import *
 from math import sqrt
 from random import shuffle
+import winsound
 HEIGHT = 768
 WIDTH = 1366
 window = Tk()
-colors = ['darkred', 'green', 'blue', 'purple', 'pink', 'lime']
+colors = ["darkred", "green", "blue", "purple", "pink", "lime"]
 health = {
-    'ammount': 3,
-    'color':'green'
+    "ammount" : 3,
+    "color": "green"
 }
-window.title('Bubble Game')
-c = Canvas(window, height = HEIGHT, width = WIDTH, bg = 'skyblue')
+window.title("Bubble Game")
+c = Canvas(window, width=WIDTH, height=HEIGHT, bg="skyblue")
 c.pack()
-ship_id = c.create_polygon(5, 5, 5, 25, 30, 15, fill='green')
-ship_id2 = c.create_oval(0, 0, 30, 30, outline = 'red')
+ship_id = c.create_polygon(5, 5, 5, 25, 30, 15, fill="green")
+ship_id2 = c.create_oval(0, 0, 30, 30, outline="red")
 SHIP_R = 15
 MID_X = WIDTH / 2
 MID_Y = HEIGHT / 2
@@ -22,20 +23,20 @@ c.move(ship_id2, MID_X, MID_Y)
 ship_spd = 10
 score = 0
 def move_ship(event):
-    if event.keysym == 'Up': #вверх
+    if event.keysym == "Up":
         c.move(ship_id, 0, -ship_spd)
         c.move(ship_id2, 0, -ship_spd)
-    elif event.keysym == 'Down':#вниз
+    elif event.keysym == "Down":
         c.move(ship_id, 0, ship_spd)
         c.move(ship_id2, 0, ship_spd)
-    elif event.keysym == 'Left':#влево
+    elif event.keysym == "Left":
         c.move(ship_id, -ship_spd, 0)
-        c.move(ship_id2, -ship_spd, 0)
-    elif event.keysym == 'Right':#вправо
+        c.move(ship_id2,  -ship_spd, 0)
+    elif event.keysym == "Right":
         c.move(ship_id, ship_spd, 0)
-        c.move(ship_id2, ship_spd, 0)
-    elif event.keysym == 'c':#чит
-        score += 10000        
+        c.move(ship_id2,  ship_spd, 0)
+    elif event.keysym == "P":
+        score += 10000
 c.bind_all('<Key>', move_ship)
 from random import randint
 bub_id = list()
@@ -52,26 +53,26 @@ def create_bubble():
     x = WIDTH + gap
     y = randint(0, HEIGHT)
     r = randint(min_bub_r, max_bub_r)
-    id1 = c.create_oval(x-r, y-r, x+r, y+r, outline='white', fill = 'lightblue')
+    id1 = c.create_oval(x - r, y - r, x + r, y + r, outline="white", fill="lightblue")
     bub_id.append(id1)
     bub_r.append(r)
-    bub_speed.append(randint(5, max_bub_spd))    
+    bub_speed.append(randint(5, max_bub_spd))
 def create_bubble_e():
     x = WIDTH + gap
     y = randint(0, HEIGHT)
     r = randint(min_bub_r, max_bub_r)
-    id1 = c.create_oval(x - r, y - r, x + r, y + r, outline = 'black', fill = 'red')
+    id1 = c.create_oval(x - r, y - r, x + r, y + r, outline="black", fill="red")
     bub_id_e.append(id1)
     bub_r_e.append(r)
-    bub_speed_e.append(randint(6, max_bub_spd))    
+    bub_speed_e.append(randint(6, max_bub_spd))
 def create_bubble_r():
     x = WIDTH + gap
     y = randint(0, HEIGHT)
     r = randint(min_bub_r, max_bub_r)
-    id1 = c.create_oval(x - r, y - r, x + r, y + r, outline = 'white', fill=colors[0])
+    id1 = c.create_oval(x - r, y - r, x + r, y + r, outline="white", fill=colors[0])
     bub_id.append(id1)
     bub_r.append(r)
-    bub_speed.append(randint(6, max_bub_spd))   
+    bub_speed.append(randint(6, max_bub_spd))
 def move_bubbles():
     for i in range(len(bub_id)):
         c.move(bub_id[i], -bub_speed[i], 0)
@@ -98,7 +99,7 @@ def distance(id1, id2):
     x1, y1 = get_coords(id1)
     x2, y2 = get_coords(id2)
     return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-def collision():
+def  collision():
     points = 0
     for bub in range(len(bub_id) -1, -1, -1):
         if distance(ship_id2, bub_id[bub]) < (SHIP_R + bub_r[bub]):
@@ -106,30 +107,30 @@ def collision():
             del_bubble(bub)
     return points
 def cleanAll():
-    for i in range(len(bub_id), -1, -1, -1):
+    for i in range(len(bub_id) -1, -1, -1):
         x, y = get_coords(bub_id[i])
         del_bubble(i)
-def collision_e():
+def  collision_e():
     for bub in range(len(bub_id_e) -1, -1, -1):
         if distance(ship_id2, bub_id_e[bub]) < (SHIP_R + bub_r_e[bub]):
             window.destroy()
-            print('Вы были убиты красным шаром...')
-            print('У вас', score, 'очков!')
-            sleep(100)
-c.create_text(50, 30, text='SCORE', fill='white')
-st = c.create_text(50, 50, fill='white')
+            print("Вы были убиты красным шаром...")
+            print("У вас ", score, " очков!")
+            sleep(100)            
+c.create_text(50, 30, text="SCORE", fill="white")
+st = c.create_text(50, 50, fill="white")
 tt = c.create_text(100, 50, fill='white')
 def show(score):
     c.itemconfig(st, text=str(score))
 evil_bub = 50
-#самоец главное
+#самое главное
 while True:
     if randint(1, bub_chance) == 1:
         create_bubble()
     if randint(1, evil_bub) == 1:
         create_bubble_e()
     if randint(1, 100) == 1:
-        create_bubble_r()        
+        create_bubble_r()
     move_bubbles()
     collision_e()
     clean()
@@ -140,10 +141,11 @@ while True:
         if score >= 1000:
             evil_bub = 30
             bub_chance = 20
-        show(score)
-        window.update()
-        shuffle(colors)
-        sleep(0.01)
+    show(score)
+    window.update()
+    shuffle(colors)
+    sleep(0.01)
+
         
         
     
