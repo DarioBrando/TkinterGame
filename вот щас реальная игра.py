0681f197b6bec0fc4445eb89c3b255db74440f92
DarioBrando <kirilleven@mail.ru>
2019-08-29 22:31:14 +0300

    Update вот щас реальная игра.py

--- вот щас реальная игра.py
+++ вот щас реальная игра.py
@@ -47,7 +47,7 @@ max_bub_spd = 10
 gap = 100
 def create_bubble():
     x = WIDTH + gap
-    y = randint(0, HEIGTH)
+    y = randint(0, HEIGHT)
     r = randint(min_bub_r, max_bub_r)
     id1 = c.create_oval(x-r, y-r, x+r, y+r, outline='white', fill = 'lightblue')
     bub_id.append(id1)
@@ -76,67 +76,66 @@ def move_bubbles():
         c.move(bub_id_e[i], -bub_speed_e[i], 0)
 from time import sleep, time
 bub_chance = 30
-def coords(id_num):
+def get_coords(id_num):
     pos = c.coords(id_num)
     x = (pos[0] + pos[2]) / 2
     y = (pos[1] + pos[3]) / 2
     return x, y
-def minusbubble(i):
+def del_bubble(i):
     del bub_r[i]
     del bub_speed[i]
     c.delete(bub_id[i])
     del bub_id[i]
 def clean():
     for i in range(len(bub_id) -1, -1, -1):
-        x, y = coords(bub_id[i])
-        if x < -100:
-            minusbubble(i)
+        x, y = get_coords(bub_id[i])
+        if x < -gap:
+            del_bubble(i)
 def distance(id1, id2):
-    x1, y1 = coords(id1)
-    x2, y2 = coords(id2)
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    x1, y1 = get_coords(id1)
+    x2, y2 = get_coords(id2)
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 def collision():
     points = 0
     for bub in range(len(bub_id) -1, -1, -1):
         if distance(ship_id2, bub_id[bub]) < (SHIP_R + bub_r[bub]):
             points += (bub_r[bub] + bub_speed[bub])
-            minusbubble(bub)
+            del_bubble(bub)
     return points
 def cleanAll():
     for i in range(len(bub_id), -1, -1, -1):
-        x, y = coords(bub_id[i])
-        minusbubble(i)
-def collision1():
-    for bub in range(len(bub_id1) -1, -1, -1):
-        if distance(ship_id2, bub_id1[bub]) < (SHIP_R + bub_r1[bub]):
+        x, y = get_coords(bub_id[i])
+        del_bubble(i)
+def collision_e():
+    for bub in range(len(bub_id_e) -1, -1, -1):
+        if distance(ship_id2, bub_id_e[bub]) < (SHIP_R + bub_r_e[bub]):
             window.destroy()
-            print('Вы были убиты красным шаром')
+            print('Вы были убиты красным шаром...')
             print('У вас', score, 'очков!')
             sleep(100)
 c.create_text(50, 30, text='SCORE', fill='white')
 st = c.create_text(50, 50, fill='white')
-c.create_text(100, 30, text='TIME', fill='white')
 tt = c.create_text(100, 50, fill='white')
 def show(score):
     c.itemconfig(st, text=str(score))
-bad_bub = 50
+evil_bub = 50
 #самоец главное
 while True:
     if randint(1, bub_chance) == 1:
-        new_bubble()
-    if randint(1, bad_bub) == 1:
-        new_bubble1()
+        create_bubble()
+    if randint(1, evil_bub) == 1:
+        create_bubble_e()
     if randint(1, 100) == 1:
         new_bubble_r()        
-    moving()
-    collision1()
+    move_bubbles()
+    collision_e()
     clean()
     score += collision()
     if score >= 400:
-        bad_bubble = 40
+        evil_bub = 40
         bub_chance = 25
         if score >= 1000:
-            bad_bubble = 30
+            evil_bub = 30
             bub_chance = 20
         show(score)
         window.update()
